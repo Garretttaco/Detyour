92360b6a6b7d7df7c38ff233d28ab7b558a8df95
Garrett Tacoronte <garretttacoronte@gmail.com>
2015-03-28 17:29:09 -0700

    gas_station working

--- resources/views/Map.blade.php
+++ resources/views/Map.blade.php
@@ -31,7 +31,7 @@
 
 @section('main_content')
  	<div class="main-container">
-      <input id="way-input" class="controls" type="text" placeholder="Search Box">
+      <input id="way-input" class="controls" type="text" placeholder="Search detour">
  		<div id="map-canvas" class="content"></div>
  	</div>
 
@@ -43,7 +43,7 @@
 		<p class="bottom-menu">Detyour</p>
 		</div>
 		<div class="detour-menu">
-		<div class="gas menu">
+		<div class="gas menu " detour="gas_station">
 			<p>Gas Stations</p>
 		</div>
 		<div class="restaurants menu">
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="<?php echo asset('css/styles.css')?>" type="text/css">
 	<script src="<?php echo asset('./bower_components/jquery/dist/jquery.js')?>"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
-	<script src="../js/main.js"></script>
+	<script src="../js/Script.js"></script>
 </head>
 <body>
 
